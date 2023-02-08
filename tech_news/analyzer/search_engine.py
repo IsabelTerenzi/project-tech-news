de5c9285243c60cc05497ae260f83fa2f3cdac08
Isabel Terenzi <bel.terenzi@gmail.com>
2023-02-08 19:08:09 -0300

    requisito 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 7
@@ -11,7 +12,16 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        data_formatada = datetime.date.fromisoformat(date).strftime("%d/%m/%Y")
+        noticia = search_news({"timestamp": data_formatada})
+        noticias = [(news["title"], news["url"]) for news in noticia]
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    return noticias
+
+# https://medium.com/data-hackers/como-manipular-datetime-no-python-578f07b72920
 
 
 # Requisito 9
