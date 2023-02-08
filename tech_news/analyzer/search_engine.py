fc61cc111d2c5237eab38b82a576d7dc98dc22d9
Isabel Terenzi <bel.terenzi@gmail.com>
2023-02-08 18:55:45 -0300

    requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,12 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    noticia = search_news({"title": {"$regex": title, "$options": "i"}})
+    noticias = [(news["title"], news["url"]) for news in noticia]
+
+    return noticias
 
 
 # Requisito 8
